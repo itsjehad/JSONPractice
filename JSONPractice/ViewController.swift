e4abbc7c45bb98c6a7429770c07f4a1a409f0257
amjehadsarkar@route1.com <amjehadsarkar@route1.com>
2019-10-24 15:42:29 -0400

    Initial Commit

---
+++ JSONPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONPractice
+//
+//  Created by Jehad Sarkar on 2019-10-24.
+//  Copyright © 2019 itsjehad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
